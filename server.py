c1c5d206b47a7a2404cc1092cb98d27cfb3643a1
Bozhinov <marogo142005@gmail.com>
2022-09-24 10:23:51 +0300

    [FINISH] DigOcean integration

--- server.py
+++ server.py
@@ -1,11 +1,22 @@
 from nextcord import Interaction, SlashOption
 from nextcord.ext import commands
 import os 
+from dotenv import load_dotenv
 
+load_dotenv()
 TOKEN = str(os.getenv('TOKEN'))
-GLOBAL_GUILD_ID = 995681969478312066  # Replace with your global guild id
+GLOBAL_GUILD_ID = int(os.getenv('GUILD_ID'))  # Replace with your global guild id
 bot = commands.Bot()
 
+@bot.event
+async def on_ready():
+    guild = bot.get_guild(GLOBAL_GUILD_ID)
+
+    print(
+        f"{bot.user} is connected to the following guild:\n"
+        f"{guild.name}(id: {guild.id})\n"
+        f"*** Bot is up-and-running ***"
+    )
 
 # command will be global if guild_ids is not specified
 @bot.slash_command(guild_ids=[GLOBAL_GUILD_ID], description="Ping command")
